2e5d7fa7cd17bf9f0ce8e5c27e46a248c58227a1
Bruno Olympio <b.olympio@reply.com>
2021-12-28 11:30:50 -0300

    mvc

---
+++ BaltaStore.Api/Controllers/HomeController.cs
@@ -0,0 +1,19 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace BaltaStore.Api.Controllers
+{
+    
+    public class HomeController : Controller
+    {
+        [HttpGet]
+        [Route("rota/01")]
+        public object Get()
+        {
+            return new { version = "Version 0.0.1" };
+        }
+    }
+}
--- BaltaStore.Api/Startup.cs
+++ BaltaStore.Api/Startup.cs
@@ -2,10 +2,6 @@
 using Microsoft.AspNetCore.Hosting;
 using Microsoft.AspNetCore.Http;
 using Microsoft.Extensions.DependencyInjection;
-using System;
-using System.Collections.Generic;
-using System.Linq;
-using System.Threading.Tasks;
 
 namespace BaltaStore.Api
 {
@@ -15,20 +11,16 @@ namespace BaltaStore.Api
         // For more information on how to configure your application, visit https://go.microsoft.com/fwlink/?LinkID=398940
         public void ConfigureServices(IServiceCollection services)
         {
+            services.AddMvc();
         }
 
         // This method gets called by the runtime. Use this method to configure the HTTP request pipeline.
         public void Configure(IApplicationBuilder app, IHostingEnvironment env)
         {
             if (env.IsDevelopment())
-            {
                 app.UseDeveloperExceptionPage();
-            }
 
-            app.Run(async (context) =>
-            {
-                await context.Response.WriteAsync("Hello World!");
-            });
+            app.UseMvc();
         }
     }
 }
